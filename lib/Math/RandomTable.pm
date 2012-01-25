c1101a95d0e815d641919ba003dce978b6d6166a
Masayuki Matsuki <y.songmu@gmail.com>
2012-01-25 16:04:19 +0900

    initial commit

---
+++ lib/Math/RandomTable.pm
@@ -0,0 +1,32 @@
+package Math::RandomTable;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Math::RandomTable -
+
+=head1 SYNOPSIS
+
+  use Math::RandomTable;
+
+=head1 DESCRIPTION
+
+Math::RandomTable is
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
